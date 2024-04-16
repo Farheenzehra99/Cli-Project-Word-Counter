481d14aa841daca57eac21ccf6e8fccdedde9889
Farheenzehra99 <farheenzehra481@gmail.com>
2024-04-17 00:08:47 +0500

    word counter project completed

--- main.js
+++ main.js
@@ -1,26 +1,12 @@
-// import inquirer from 'inquirer';
-// const answers: {
-//     Sentence:string
-// } = await inquirer.prompt([
-//     {
-//         name: "Sentence",
-//         type: "input",
-//         message: "Enter your sentence to count the word count: "
-//     }
-// ])
-// const words = answers.Sentence.trim().split(" ")
-// // console.log(words)
-// console.log(`Your sentence word count is ${words.length}`)
+#! /usr/bin/env Node
 import inquirer from "inquirer";
-const counter = (paragraph) => paragraph.replace(/\s/g, "").length;
-async function startWordCounter(counter) {
-    do {
-        let res = await inquirer.prompt({
-            name: "paragraph",
-            type: "input",
-            message: "Enter your sentence to count the word count: "
-        });
-        console.log(counter(res.paragraph));
-    } while (true);
-}
-startWordCounter(counter);
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word count: "
+    }
+]);
+const words = answers.Sentence.trim().split(" ");
+console.log(words);
+console.log(`Your sentence word count is ${words.length}`);
--- main.ts
+++ main.ts
@@ -1,19 +1,18 @@
 #! /usr/bin/env Node
 import inquirer from "inquirer";
 
-const counter =(paragraph: string)=>paragraph.replace(/\s/g,"").length
 
-
-async function startWordCounter(counter:(text:string)=>number){
-    do{
-    let res = await inquirer.prompt({
-        name: "paragraph",
+const answers: {
+    Sentence:string
+} = await inquirer.prompt([
+    {
+        name: "Sentence",
         type: "input",
         message: "Enter your sentence to count the word count: "
-    })
-     console.log(counter(res.paragraph))
-}
-while(true)
-}
+    }
+])
 
-startWordCounter(counter)
+const words = answers.Sentence.trim().split(" ")
+console.log(words)
+
+console.log(`Your sentence word count is ${words.length}`)
